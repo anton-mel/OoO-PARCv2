dbd076667e344aa019198ca1715117b806d370a3
anton-mel <anton.melnychuk3@gmail.com>
2025-03-21 12:11:52 -0400

    Add VMH Files, Test/Build [Remove if Needed]

---
+++ cpsc420-lab3/build/compile_test.sh
@@ -0,0 +1,11 @@
+make clean
+rm -rf ../tests/build/bin
+rm -rf ../tests/build/dep
+rm -rf ../tests/build/vmh
+rm -rf ../tests/build/dump
+cd ../tests/build
+make clean
+make
+../convert
+cd ../../build
+make check-asm-pv2stall check-asm-pv2byp check-asm-pv2long check-asm-rand-pv2long
