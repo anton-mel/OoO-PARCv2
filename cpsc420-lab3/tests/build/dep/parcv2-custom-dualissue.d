7bb60043fb622e148b9991302684ceee4a0347df
anton-mel <anton.melnychuk3@gmail.com>
2025-04-06 20:58:11 -0400

    Add the Tests

---
+++ cpsc420-lab3/tests/build/dep/parcv2-custom-dualissue.d
@@ -0,0 +1,4 @@
+parcv2-custom-dualissue: ../parcv2/parcv2-custom-dualissue.S \
+ ../parcv2/parc-macros.h
+
+../parcv2/parc-macros.h:
---
+++ cpsc420-lab3/tests/build/dep/parcv2-custom-waw.d
@@ -0,0 +1,3 @@
+parcv2-custom-waw: ../parcv2/parcv2-custom-waw.S ../parcv2/parc-macros.h
+
+../parcv2/parc-macros.h:
